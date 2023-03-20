e1f79bbf5f23265ce11d66ef124dc5338db871bc
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2023-03-20 13:08:54 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,83 @@
+import socket
+from threading import Thread
+import random
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 1234
+
+server.bind((ip_address, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+questions = [
+   "Biggest planet in our solar system", "Smallest city", "Capital of France"
+]
+
+answers = [
+    "Jupiter", "Vatican City", "Paris"
+]
+
+print("Server has started...")
+
+def get_random_question_answer(conn):
+    random_index = random.randint(0,len(questions) - 1)
+    random_question = questions[random_index]
+    random_answer = answers[random_index]
+    conn.send(random_question.encode('utf-8'))
+    return random_index, random_question, random_answer
+
+def remove_question(index):
+    questions.pop(index)
+    answers.pop(index)
+
+def clientthread(conn, nickname):
+    score = 0
+    conn.send("Welcome to this quiz game!".encode('utf-8'))
+    conn.send("You will receive a question.".encode('utf-8'))
+    conn.send("Good Luck!\n\n".encode('utf-8'))
+    index, question, answer = get_random_question_answer(conn)
+    print(answer)
+    while True:
+        try:
+            message = conn.recv(2048).decode('utf-8')
+            if message:
+                if len(questions)!=0:
+                    if message.split(": ")[-1].lower() == answer:
+                        score += 1
+                        conn.send(f"Nice! Your score is {score}\n\n".encode('utf-8'))
+                    else:
+                        conn.send("Incorrect answer! Better luck next time!\n\n".encode('utf-8'))
+                else:
+                    break
+                            
+                remove_question(index)
+                index, question, answer = get_random_question_answer(conn)
+                print(answer)
+            else:
+                remove(conn)
+                remove_nickname(nickname)
+        except Exception as e:
+            print(str(e))
+            continue
+
+def remove(connection):
+    if connection in clients:
+        clients.remove(connection)
+
+def remove_nickname(nickname):
+    if nickname in nicknames:
+        nicknames.remove(nickname)
+
+while True:
+    conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
+    clients.append(conn)
+    nicknames.append(nickname)
+    print (nickname + " connected!")
+    new_thread = Thread(target= clientthread,args=(conn,nickname))
+    new_thread.start()
